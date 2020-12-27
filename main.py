496cf24a049302b6ae41ee80ec54a317a852c695
José Figueredo <josemafl14@gmail.com>
2020-12-27 01:07:19 +0000

    basic bot done

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import discord
+import os
 
+client = discord.Client()
+
+@client.event
+
+#ni idea
+async def on_ready():
+  print("Logged in as {0.user}".format(client))
+
+@client.event 
+
+#help command
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith(".help"):
+    await message.channel.send("[descripcion del bot]\n\nComandos\n.spam [usuario]") 
+    
+client.run(os.getenv("token"))
